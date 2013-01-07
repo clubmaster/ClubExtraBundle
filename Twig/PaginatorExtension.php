6964cb8032d9b08eff8728a2d423612f079b622a
Michael Holm <hollo@hollo.dk>
2013-01-07 21:38:16 +0100

    Added twig function for paginator.

---
+++ Twig/PaginatorExtension.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Club\ExtraBundle\Twig;
+
+class PaginatorExtension extends \Twig_Extension
+{
+    private $container;
+
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
+
+    public function getFunctions()
+    {
+        return array(
+            'club_paginator' => new \Twig_Function_Method($this, 'render')
+        );
+    }
+
+    public function render()
+    {
+        $nav = $this->container->get('club_extra.paginator');
+
+        return $this->container->get('templating')->render('ClubExtraBundle:Paginator:paginator.html.twig', array(
+            'nav' => $nav
+        ));
+    }
+
+    public function getName()
+    {
+        return 'club_paginator';
+    }
+}
